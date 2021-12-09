d172307946ba3d1cd44edb7de02597f4c3c3d364
Joseph Guzzardo <joseph.a.guzzardo@gmail.com>
2021-12-09 11:38:33 -0500

    Welcome now has welcome title and body with some text styling
    Text and styling added, still needs adjustment for layout

--- components/welcome/Welcome.js
+++ components/welcome/Welcome.js
@@ -4,34 +4,41 @@
 */
 //Imports
 import React from "react";
-import {ImageBackground, Image, Button, StyleSheet} from 'react-native'
+import {ImageBackground, Button, StyleSheet,Text} from 'react-native'
 
 //==IMG== Photo of monstera plant used for background of welcome screen
 import monsteraBackground from './images/gilles-lambert-mSK5nNsAsLY-unsplash.jpg';
 
 const Welcome = ({ navigation }) => {
+
+  const welcomeTitle = "Welcome";
+  const welcomeBody = "A social media plantfrom aimed at sharing the growth of your plants with the world!";
     return (
       <>
         <ImageBackground source={monsteraBackground} resizeMode="cover" style={styles.image}>
         {/* <Image source={require('./images/gilles-lambert-mSK5nNsAsLY-unsplash.jpg')} style={styles.img}/> */}
-        <Button
-        title="Go to Main Feed"
-        onPress={() =>
-          navigation.navigate('MainFeed', { name: 'Main Feed' })
-        }
-        />
-        <Button
-        title="Login"
-        onPress={() =>
-          navigation.navigate('Login', { name: 'Login' })
-        }
-        />
-        <Button
-        title="Create An Account"
-        onPress={() =>
-          navigation.navigate('Sign Up', { name: 'Create Account' })
-        }
-        />
+        <Text style={styles.title}>{welcomeTitle}</Text>
+        <Text style={styles.body}>{welcomeBody}</Text>
+        <>
+          <Button
+          title="Go to Main Feed"
+          onPress={() =>
+            navigation.navigate('MainFeed', { name: 'Main Feed' })
+          }
+          />
+          <Button
+          title="Login"
+          onPress={() =>
+            navigation.navigate('Login', { name: 'Login' })
+          }
+          />
+          <Button
+          title="Create An Account"
+          onPress={() =>
+            navigation.navigate('Sign Up', { name: 'Create Account' })
+          }
+          />
+        </>
         </ImageBackground>
       </>
     );
@@ -42,6 +49,17 @@ const Welcome = ({ navigation }) => {
         flex: 1,
         justifyContent: "center"
       },
+    title: {
+      color: 'white',
+      textAlign: 'center',
+      fontSize: 30,
+    },
+    body: {
+      color: 'white',
+      textAlign: 'center',
+      fontSize: 20,
+      paddingBottom: 20
+    }
   });
 
 
